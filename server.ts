d367be1788216bc20e5bbc28b37478c5a4587c92
Mathias Erligmann <merligmann@gmail.com>
2020-06-25 00:40:05 +0200

    new port

--- server.ts
+++ server.ts
@@ -12,8 +12,8 @@ const server = new Drash.Http.Server({
 });
 
 server.run({
-  hostname: "localhost",
-  port: 8080,
+  hostname: "127.0.0.1",
+  port: 3000,
 });
 
 console.log("Server listennig on port " + server.port);
